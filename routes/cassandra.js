8d35b4b77c74a9d5320bfc5d840518938d4b0fbb
root <root@project-hornet-app.localdomain>
2017-04-20 17:31:06 +0000

    commit from server

--- routes/cassandra.js
+++ routes/cassandra.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
 const id = cassandra.types.Uuid.random();
-var size = 100000;
+
 var start, finish;
 
 
@@ -17,15 +17,22 @@ var router = function (app) {
     app.post('/cassandra-api/data', function (req, res) {
         client.connect()
             .then(function () {
+	            var size = 1;
+	            if(!req.body.txt || !req.body.val || !req.body.date){
+					return res.status(400).json({message: 'Please fill out all fields'});
+        		}
                 var query = 'INSERT INTO examples.basic (id, txt, val, date) VALUES (?, ?, ?, ?)';
                 start = new Date();
                 console.log("executing my stuff");
-                for (var num = 1; num <= size; num++) {
+                /*for (var num = 1; num <= size; num++) {
                     client.execute(query, [cassandra.types.Uuid.random(), 'Hello!', num, new Date()], { prepare: true }, function (err) {
-                        //console.log("WDF");
                         assert.ifError(err);
                     });
-                }
+                }*/
+                client.execute(query, [cassandra.types.Uuid.random(),req.body.txt , req.body.val, req.body.date], { prepare: true }, function (err) 				{
+                    assert.ifError(err);
+                });
+
                 finish = new Date();
                 var time = (finish.getTime() - start.getTime()) + " ms";
                 console.log("Operation took " + time);
