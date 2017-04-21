e06d0028dbb9b07653ea82146f3c7b930737a592
parzie <plclavijop@gmail.com>
2017-04-21 12:29:42 -0500

    test with bulk

--- mongo_app.js
+++ mongo_app.js
@@ -22,16 +22,30 @@ var insertDocument = function (db, num, callback) {
   });
 };
 
+var insertDocument = function (bulk, num, callback) {
+  bulk.insert({
+    "txt": 'Hello!',
+    "val": num,
+    "date": new Date()
+  }, function (err, result) {
+    assert.equal(err, null);
+    callback();
+  });
+};
+
 
 MongoClient.connect(url, function (err, db) {
   start = new Date();
   console.log("executing my stuff");
   assert.equal(null, err);
+  var bulk = db.collection('basic').initializeUnorderedBulkOp();
+  
   for (var num = 0; num < size; num++) {
-    insertDocument(db, num, function(){
-      db.close();
-    });
+    insertDocument(bulk, num);//, function(){
+      //db.close();
+    //});
   }
+  bulk.execute();
   finish = new Date();
   console.log("Operation took " + (finish.getTime() - start.getTime()) + " ms");
 
