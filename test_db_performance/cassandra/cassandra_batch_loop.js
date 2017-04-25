8b0092e5c6fc885d9a20c03085c4b6af03d6c78f
parzie <plclavijop@gmail.com>
2017-04-25 15:47:26 -0500

    batch insert with cassandra

--- test_db_performance/cassandra/cassandra_batch_loop.js
+++ test_db_performance/cassandra/cassandra_batch_loop.js
@@ -3,9 +3,8 @@ const cassandra = require('cassandra-driver');
 const async = require('async');
 const assert = require('assert');
 
-const client = new cassandra.Client({ contactPoints: ['127.0.0.1']});
-var num = 111;
-var size = 100000;
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
+var size = 65000;
 var start, finish;
 /**
  * Example using async library for avoiding nested callbacks
@@ -15,33 +14,38 @@ var start, finish;
 //const id = cassandra.types.Uuid.random();
 
 async.series([
-  function connect(next) {
-    client.connect(next);
-  },
-  function createKeyspace(next) {
-    var query = "CREATE KEYSPACE IF NOT EXISTS examples WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' }";
-    client.execute(query, next);
-  },
-  function createTable(next) {
-    var query = "CREATE TABLE IF NOT EXISTS examples.basic (id uuid, txt text, val int, date timestamp , PRIMARY KEY(id))";
-    client.execute(query, next);
-  },
-  function insert(next) {
-    var query = 'INSERT INTO examples.basic (id, txt, val, date) VALUES (?, ?, ?, ?)';
-    start = new Date();
-    console.log("executing my stuff");
-    for (var num = 1; num <= size; num++) {
-      client.batch(query, [cassandra.types.Uuid.random(), 'Hello!', num, new Date()], { prepare: true },  function(err){
-        assert.ifError(err);
-    });
+    function connect(next) {
+        client.connect(next);
+    },
+    function createKeyspace(next) {
+        var query = "CREATE KEYSPACE IF NOT EXISTS examples WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' }";
+        client.execute(query, next);
+    },
+    function createTable(next) {
+        var query = "CREATE TABLE IF NOT EXISTS examples.basic (id uuid, txt text, val int, date timestamp , PRIMARY KEY(id))";
+        client.execute(query, next);
+    },
+    function insert(next) {
+        var query = 'INSERT INTO examples.basic (id, txt, val, date) VALUES (?, ?, ?, ?)';
+        start = new Date();
+        console.log("executing my stuff");
+
+        var queries = [];
+        for (var num = 1; num <= size; num++) {
+            queries.push({"query":query, "params":[cassandra.types.Uuid.random(), 'Hello!', num, new Date()]});
+        }
+
+        client.batch(queries, { prepare: true }, function (err) {
+                assert.ifError(err);
+        });
+            //console.log("queries: ",queries);
+        finish = new Date();
+        console.log("Operation took " + (finish.getTime() - start.getTime()) + " ms");
     }
-    finish = new Date();
-    console.log("Operation took " + (finish.getTime() - start.getTime()) + " ms");
-  }
 ], function (err) {
-  if (err) {
-    console.error('There was an error', err.message, err.stack);
-  }
-  console.log('Shutting down');
-  client.shutdown();
+    if (err) {
+        console.error('There was an error', err.message, err.stack);
+    }
+    console.log('Shutting down');
+    client.shutdown();
 });
