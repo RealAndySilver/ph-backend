6983b3fbee1ead13f4d77ac2edcb6b18ccc3809d
parzie <plclavijop@gmail.com>
2017-04-26 16:03:01 -0500

    expand limit to 50mb

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var app = express();
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+app.use(bodyParser({limit: '50mb'})); 
+
 var mongo = require("./routes/mongo.js")(app);
 var cassandra = require("./routes/cassandra.js")(app);
 
