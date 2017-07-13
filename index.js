23484ca45401e92d7f1d7af28508923bc6ba063d
Andres Abril <andres.abril@gmail.com>
2017-07-12 21:01:51 -0700

    Added cross origin to index.js file

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UNLOCK,PURGE');
+    res.header(	'Access-Control-Allow-Headers', 
+    			'Content-Type , content-type, Authorization, Content-Length, X-Requested-With, type, token,Cache-Control,If-Modified-Since,if-modified-since, pragma');
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else{
+	  next();  
+    }
+}
+
+app.use(allowCrossDomain);
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(express.static(__dirname + '/public'));
@@ -12,3 +27,23 @@ var mongo = require("./routes/mongo.js")(app,io);
 var cassandra = require("./routes/cassandra.js")(app);
 
 server.listen(port, () => console.log('listening on port ' + port));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//******************************* IMPORTANT ******* IMPORTANT *******************************************//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////// Garbage collector, server need to be started with the command ' node --expose-gc ./bin/www '///////
+/////// This command has been added to the package.json scripts and can be started as ' npm start ' ///////
+///////////// This manual garbage collection helps to reduce the footprint significantly //////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//******************************* IMPORTANT ******* IMPORTANT *******************************************//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+setInterval(function(){
+  //Garbage collection every 5 seconds.
+  global.gc();
+  //console.log('GC done mem:',process.memoryUsage());
+}, 1000*5);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// End of Garbage Collection ///////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
